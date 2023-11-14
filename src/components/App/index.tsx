e3649d43f66514ff955dbaa5d553b272987b2558
xitaozhu <xzhu21@student.ubc.ca>
2023-11-13 17:57:28 -0800

    Add header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,9 +2,11 @@ import './index.scss';
 
 import React from 'react';
 
+import Main from '../Main';
+
 const App: React.FC = (): JSX.Element => (
     <div className="App">
-        <h1>Hello World</h1>
+        <Main />
     </div>
 );
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import './index.scss';
+import 'dayjs/locale/zh-cn';
+
+import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
+
+import Icon from '../../lib/Icon';
+
